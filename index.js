97dbfa1995c0905be26b09d07398be3049c1555e
Jackie Galarza <jcqlnray@gmail.com>
2019-03-25 21:45:16 -0400

    added weather fetch and display

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 'use strict';
 
-// const SEARCH_RADIUS = 16000;
-const API_KEY_STORMGLASS = "7f249d00-4b47-11e9-869f-0242ac130004-7f249e0e-4b47-11e9-869f-0242ac130004";
 const API_KEY_NP = "Zr0S27a7DvoEnaDaAY30QC6Lehvz1lg7YITO8OKf";
-// const LOCATION = "";
-
+const API_KEY_STORMGLASS = "7f249d00-4b47-11e9-869f-0242ac130004-7f249e0e-4b47-11e9-869f-0242ac130004";
 
 function formatQueryParams(params) {
   const queryItems = Object.keys(params)
@@ -24,26 +21,26 @@ function getPark(searchTerm) {
 
   fetch(parksUrl)
     .then(response => {
-      // console.log(response);
       if (response.ok) {
         return response.json();
       }
       throw new Error(response.statusText);
     })
+    // make sure results contain only the named park
     .then(responseJson => {
       const park = responseJson.data.find(item => item.fullName.toLowerCase().includes(searchTerm.toLowerCase()))
       console.log(park)
       return park;
-      }) 
+      })
     .then(park => getPoint(park))
     .catch(err => {
       console.error(err);
       $('#js-error-message').text(`Something went wrong: ${err.message}`);
     });
-
   }
 
-// Get the coordinates from the National Parks API data
+// Get park coordinates and weather summary from the National Parks API data. Then call functions to  (1)display the summary and (2)get current weather data from StormGlass API using coordinates
+
  function getPoint(park){
   let parkCoords= park.latLong.split(",");
     // console.log(parkCoords);
@@ -54,27 +51,46 @@ function getPark(searchTerm) {
   let lat = cords[0][1];
     // console.log(lat);
   let lng = cords[1][1];
-    // console.log(lng);
+    // console.log(lng)
+  let summary = park.weatherInfo;
+  displaySummary(summary);
   getWeather(lat, lng);
  }
 
-// Fetch Weather data based on selected park coordinates
+ function displaySummary(summary) {
+   $('#summary').append(
+      `<p>${summary}</p>`);
+ }
+
+// Fetch weather data with selected park's coordinates
 function getWeather(lat,lng) {
 
-  const params = 'airTemperature,cloudCover,precipitation';
+  const params = 'airTemperature,cloudCover';
 
   const options = {
     headers: new Headers({
       "Authorization": API_KEY_STORMGLASS })
   };
   
-// let nowTime = Date.time();
-let d = new Date();
-let t =  d.setUTCHours(12);
+let dateTime = new Date();
+dateTime.setMinutes(0);
+dateTime.setSeconds(0);
+dateTime.setMilliseconds(0);
+
+let startTime =  dateTime.toISOString();
+console.log("Start Time: " + startTime);
+
+let dateTime2 = new Date();
+// let tzo2 = dateTime2.getTimezoneOffset();
+dateTime2.setMinutes(0);
+dateTime2.setSeconds(0);
+dateTime2.setMilliseconds(0);
+let endTime = dateTime2.toISOString();
+console.log("End Time:" + endTime);
   // const queryString = formatQueryParams(params)
   // const url = searchURL + '?' + queryString;
   const url = 
-`https://api.stormglass.io/point?lat=${lat}&lng=${lng}&params=${params}&source=noaa&start=${t}&end=${t}`;
+`https://api.stormglass.io/point?lat=${lat}&lng=${lng}&params=${params}&source=noaa&start=${startTime}&end=${endTime}`;
   console.log(url);
 
   fetch(url, options)
@@ -91,6 +107,10 @@ let t =  d.setUTCHours(12);
     });
 }
 
+function toFahrenheit(celsius) {
+    let fahrenheit = Math.round((celsius * (9/5)) + 32);
+    return fahrenheit;
+}
 
 // Add content to Page
 
@@ -100,16 +120,19 @@ function displayResults(responseJson) {
   // if there are previous results, remove them
   $('li').remove();
 
-  // iterate through the response data array
-  // for (let i = 1; i < responseJson.data.length; i++){
+  let celsius = responseJson.hours[0].airTemperature[0].value;
+  let degreesFar = toFahrenheit(celsius);
 
-  //   $('#results-list').append(
-  //     `<li><h3>${responseJson.data[i].fullName}</h3>
-  //     <p>${responseJson.data[i].description}</p>
-  //     <a href=${responseJson.data[i].url}>${responseJson.data[i].url}</a>
-  //     </li>`
-  //   )};
+  // iterate through the response data array
+  // for (let i = 1; i < responseJson.length; i++){
+
+    $('#results-list').append(
+      `<li><p>Temperature: ${degreesFar} ^F</p></li>
+      <li><p>Cloud Cover: ${responseJson.hours[0].cloudCover[0].value}%</p></li>
+      `
+    );
 };
+// };
 
 // Event Listeners
   // get the park name from input field
